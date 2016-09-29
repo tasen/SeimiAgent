62bf939ac0bc718a0a876a8cc602461efd4ea415
tasen <tasen.w@gmail.com>
2016-09-29 19:58:44 +0800

    test

--- src/SeimiServerHandler.cpp
+++ src/SeimiServerHandler.cpp
@@ -19,11 +19,15 @@
 #include <QRegularExpression>
 #include <QRegularExpressionMatch>
 #include <QBuffer>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QTime>
 #include "SeimiServerHandler.h"
 #include "SeimiWebPage.h"
 #include "pillowcore/HttpServer.h"
 #include "pillowcore/HttpHandler.h"
 #include "pillowcore/HttpConnection.h"
+
 
 SeimiServerHandler::SeimiServerHandler(QObject *parent):Pillow::HttpHandler(parent),
     renderTimeP("renderTime"),
@@ -39,23 +43,44 @@ SeimiServerHandler::SeimiServerHandler(QObject *parent):Pillow::HttpHandler(pare
 }
 
 bool SeimiServerHandler::handleRequest(Pillow::HttpConnection *connection){
+    QTime qTime;
+    qTime.start();
+
     QString method = connection->requestMethod();
     QString path = connection->requestPath();
     if(method == "GET"){
         connection->writeResponse(405, Pillow::HttpHeaderCollection(),"Method 'GET' is not supprot,please use 'POST'");
         return true;
     }
-    if(path != "/doload"){
-        return false;
+    if(path != "/" and path != ""){
+        connection->writeResponse(404, Pillow::HttpHeaderCollection(),"page not found!");
+        return true;
     }
     QEventLoop eventLoop;
     SeimiPage *seimiPage=new SeimiPage(this);
-    QString url = QUrl::fromPercentEncoding(connection->requestParamValue(urlP).toUtf8());
-    int renderTime = connection->requestParamValue(renderTimeP).toInt();
-    QString proxyStr = connection->requestParamValue(proxyP);
-    QString contentType = connection->requestParamValue(contentTypeP);
-    QString outImgSizeStr = connection->requestParamValue(outImgSizeP);
+
+    //获取请求内容，便于使用json解析
+    QString reqContent = connection->requestContent();
+    QJsonParseError json_error;
+    QJsonDocument jDoc = QJsonDocument::fromJson(reqContent.toStdString().c_str(), &json_error);
+    if(json_error.error != QJsonParseError::NoError or !jDoc.isObject()){
+        qInfo()<<"[Seimi] parameters must be json format［Params = "+reqContent+"］";
+        connection->writeResponse(501, Pillow::HttpHeaderCollection(),"parameters must be json format!");
+        return true;
+    }
+    QJsonObject params = jDoc.object();
+
+    qInfo()<<"[Seimi] Params:"+reqContent;
+
+    QString url = QUrl::fromPercentEncoding(params.take("url").toString().toUtf8());
+    int renderTime = params.take("wait").toInt()*1000;
+    QString proxyStr = params.take("proxy").toString();
+    QString contentType = params.take("contentType").toString();
+    QString outImgSizeStr = params.take("outImgSize").toString();
     if(!proxyStr.isEmpty()){
+        if(proxyStr.indexOf("://") == -1){
+            proxyStr = "http://" + proxyStr;
+        }
         QRegularExpression reProxy("(?<protocol>http|https|socket)://(?:(?<user>\\w*):(?<password>\\w*)@)?(?<host>[\\w.]+)(:(?<port>\\d+))?");
         QRegularExpressionMatch matchProxy = reProxy.match(proxyStr);
         if(matchProxy.hasMatch()){
@@ -76,12 +101,21 @@ bool SeimiServerHandler::handleRequest(Pillow::HttpConnection *connection){
         }
     }
 
-    QString jscript = QUrl::fromPercentEncoding(connection->requestParamValue(scriptP).toUtf8());
+    //QString jscript = QUrl::fromPercentEncoding(connection->requestParamValue(scriptP).toUtf8());
+    QString jscript = "(" + QUrl::fromPercentEncoding(params.take("js_script").toString().toUtf8()) +")()";
+    if(jscript == "()()"){
+        jscript = "";
+    }
 //    qDebug()<<"recive js:"<<jscript;
-    QString postParamJson = connection->requestParamValue(postParamP);
+    QString postParamJson = "{\"" + params.take("data").toString().replace("=", "\":\"").replace("&", "\",\"") +"\"}";
+    if(postParamJson == "{\"\"}"){
+        postParamJson = "";
+    }else{
+        qInfo()<<"[Seimi] postParamJson:" + postParamJson;
+    }
     seimiPage->setScript(jscript);
     seimiPage->setPostParam(postParamJson);
-    qInfo("[seimi] TargetUrl:%s ,RenderTime(ms):%d",url.toUtf8().constData(),renderTime);
+    qInfo("[Seimi] TargetUrl:%s ,RenderTime(ms):%d",url.toUtf8().constData(),renderTime);
     int useCookieFlag = connection->requestParamValue(useCookieP).toInt();
     seimiPage->setUseCookie(useCookieFlag==1);
     QObject::connect(seimiPage,SIGNAL(loadOver()),&eventLoop,SLOT(quit()));
@@ -118,9 +152,24 @@ bool SeimiServerHandler::handleRequest(Pillow::HttpConnection *connection){
         connection->writeResponse(200,headers,imgContent);
     }else{
         headers << Pillow::HttpHeader("Content-Type", "text/html;charset=utf-8");
-        QString defBody = "<html>null</html>";
-        qInfo()<<"content:"<<seimiPage->getContent();
-        connection->writeResponse(200, headers,seimiPage->getContent().isEmpty()?defBody.toUtf8():seimiPage->getContent().toUtf8());
+        QString defBody = "{\n\t\"content\":\"null\"\n}";
+        QString content = seimiPage->getContent();
+        qInfo()<<"[Seimi] content:"<<content.replace("\n", "\\n").left(200) + " ...";
+
+        int timeDiff = qTime.elapsed();
+        float f = timeDiff/1000.0;
+        QString time = QString("%2").arg(f);
+
+        QString data = "{\n\t\"content\":\"" + content.replace("\"", "\\\"") + "\",\n"
+                       + "\t\"js_script_result\":\"" + seimiPage->getJSResult() + "\"" + ",\n"
+                       + "\t\"orig_url\":\"" + url + "\"" + ",\n"
+                       + "\t\"url\":\"" + url + "\"" + ",\n"
+                       + "\t\"status_code\":\"200\"" + ",\n"
+                       + "\t\"cookies\":\"" + "" + "\"" + ",\n"
+                       + "\t\"cookiesString\":\"" + "" + "\"" + ",\n"
+                       + "\t\"time\":" + time
+                       + "\n}";
+        connection->writeResponse(200, headers,seimiPage->getContent().isEmpty()?defBody.toUtf8():data.toUtf8());
     }
     seimiPage->deleteLater();
     return true;
